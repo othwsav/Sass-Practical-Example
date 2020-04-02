67500dd3c57f6224a3bd6e0e0b33d5a4e5026039
othwsav <54482114+othwsav@users.noreply.github.com>
2020-04-02 05:07:37 +0100

    I added a theme switcher

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+    // slider script
     $("section.slider ul.bullets li").on('click', function(){
         $(this).addClass("active").siblings().removeClass("active");
         let thatNum = $(this).index() + 1;
@@ -9,5 +10,112 @@ $(document).ready(function (){
                 opacity : '1'
             },500)
         })
+    })
+    // header menu script
+    let after = false
+    $(window).scroll(function (){
+        if($(window).scrollTop() >= $("section.slider").outerHeight()){
+            if(after == false){
+                $("header.header.notm").addClass("fixedheader")
+                .removeClass("topheader").offset({
+                    top: -90
+                })
+                .animate({
+                    top: 0
+                }, {
+                    duration: 700,
+                    queue: false
+                })
+
+                $("section.slider").animate({
+                    marginTop: "90px"
+                }, {
+                    duration: 700,
+                    queue: false
+                })
+
+                after = true
+            }
+        } else{
+            if(after == true){
+                $("header.header.notm").animate({
+                    top: "-90px"
+                }, {
+                    duration: 100,
+                    queue: false,
+                    complete: function (){
+                        $(this).addClass("topheader")
+                        .removeClass("fixedheader")
+                        .animate({
+                            top: 0
+                        },0)
+                    }
+                })
+
+                $("section.slider").animate({
+                    marginTop: "139px"
+                }, {
+                    duration: 100,
+                    queue: false
+                })
+
+                after = false
+            }
+        }
+    })
+    // mobile header menu
+    let arabic = location.href.includes("index-rtl.html")
+    $("header #mobile-button").on("click", function(){
+        $("#mobile-overlay").fadeIn(300)
+        if(arabic){
+            $("header.header.mobile").animate({
+                right: 0
+            }, 500)
+        } else{
+            $("header.header.mobile").animate({
+                left: 0
+            }, 500)
+        }
+    })
+    $("#mobile-overlay").on("click", function(){
+        $(this).fadeOut(300)
+        if(arabic){
+            $("header.header.mobile").css({
+                "right": "min(-35%, -300px)"
+            })
+        } else{
+            $("header.header.mobile").css({
+                "left": "min(-35%, -300px)"
+            })
+        }
+    })
+    // theme button
+    $(".theme-button").click(function () {
+        $(".colors").slideToggle(300)
+    })
+    let bgColor = $("body").css('--bgColor');
+    $(".theme-button").mouseenter(function () {
+        $(".colors").css({
+            "background": bgColor,
+            // "border": "2px solid #f6f6f6"
+        })
+
+        $(".colors").toggleClass("on")
+    })
+
+    $(".theme-button").mouseleave(function () {
+        console.log("object");
+        $(".colors").css({
+            "background": "#f6f6f6",
+            // "border": "2px solid " + bgColor
+        })
+
+        $(".colors").toggleClass("on")
+    })
+    // changing theme on click
+    $(".colors .color").on("click", function (){
+        $("body").removeClass().addClass($(this).data("color"))
+        bgColor = $("body").css('--bgColor')
+        $(".theme-button").click()
     })
 })
